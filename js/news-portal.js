92024bc49674b7feb6af9269c3844ef657266fe1
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-03-03 15:41:46 +0600

    section added

--- js/news-portal.js
+++ js/news-portal.js
@@ -30,9 +30,121 @@ const showAllNews =(data,category_name) =>{
     console.log(data,category_name);
     document.getElementById('news-count').innerText = data.length
     document.getElementById('catagory-name').innerText = category_name
+    const newsContainer = document.getElementById('all-news')
+    newsContainer.innerHTML='';
     data.forEach(singleNews => {
-        console.log(singleNews);
+        // newsContainer.innerHTML =+ ``
+
+        const{_id,image_url,title,details,author,total_view}=singleNews;
+        const card = document.createElement('div')
+        card.classList.add('çard','mb-3');
+        card.innerHTML = `
+
+    <div class="row g-0 bg-body ">
+        <div class="col-md-4">
+            <img src="${image_url}
+            " class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8 d-flex flex-column p-3">
+          <div class="card-body">
+            <h5 class="card-title">${title}
+            </h5>
+            <p class="card-text">${details.slice(0,200)}.....
+           </p>
+          
+          </div> 
+          <div class="card-footer border-0 d-flex justify-content-between  ">
+          <div class="d-flex gap-2">
+          <img src="${singleNews.author.img}
+          " class="img-fluid rounded-circle" alt="..." height="40" width="40">
+          <div>
+          <p classs = "m-0 p-0">${author.name}</p>
+          <p classs = "m-0 p-0">${singleNews.author.published_date}</p>
+          </div>
+          </div>
+         
+          <div class="d-flex align-items-center">
+                <i class ="fas fa-eye"></i>
+                <p classs = "m-0 p-0">${total_view
+                }</p>   
+          </div>
+
+          <div>
+           <i class ="fas fa-star"></i>
+           </div>
+
+          <div>
+           <i class ="fas fa-arrow-right" onclick="showNewsDetail('${_id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+           </div>
+        
+          </div>
+        </div>
+      </div>
+        `;
+        newsContainer.appendChild(card)
     })
 }
+
+const fetchNewsDetail = news_id =>{
+    
+    let url = `https://openapi.programming-hero.com/api/news/${news_id}`
+fetch (url).then(res => res.json())
+.then(data => showNewsDetail(data.data[0]))
+
+}  
+
+const showNewsDetail = newsDetail => {
+    // newsContainer.innerHTML =+ ``
+const {_id,image_url,title ,details,author, total_view,} = newsDetail;
+   
+    document.getElementById("modal-body").innerHTML = `
+    <div class='çard','mb-3'>
+    <div class="row g-0 bg-body ">
+        <div class="col-md-4">
+            <img src="${image_url}
+            " class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8 d-flex flex-column p-3">
+          <div class="card-body">
+            <h5 class="card-title">${title}
+            </h5>
+            <p class="card-text">${details}.....
+           </p>
+          
+          </div> 
+          <div class="card-footer border-0 d-flex justify-content-between  ">
+          <div class="d-flex gap-2">
+          <img src="${singleNews.author.img}
+          " class="img-fluid rounded-circle" alt="..." height="40" width="40">
+          <div>
+          <p classs = "m-0 p-0">${author.name}</p>
+          <p classs = "m-0 p-0">${singleNews.author.published_date}</p>
+          </div>
+          </div>
+         
+          <div class="d-flex align-items-center">
+                <i class ="fas fa-eye"></i>
+                <p classs = "m-0 p-0">${total_view
+                }</p>   
+          </div>
+
+          <div>
+           <i class ="fas fa-star"></i>
+           </div>
+
+          <div>
+           <i class ="fas fa-arrow-right" onclick="showNewsDetail('${_id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+           </div>
+        
+          </div>
+        </div>
+      </div>
+
+    
+    </div>
+
+    `;
+            
+}
 // fetchCatagories()
-d
+
